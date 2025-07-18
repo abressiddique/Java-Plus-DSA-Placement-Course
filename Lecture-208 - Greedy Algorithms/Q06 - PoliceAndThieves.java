0e33cfca1f17934f2c74e41fb06e0729be3c6a94
abressiddique <abressiddique@gmail.com>
2025-07-18 20:31:23 +0530

    Update Q06 - PoliceAndThieves.java

--- Lecture-208 - Greedy Algorithms/Q06 - PoliceAndThieves.java
+++ Lecture-208 - Greedy Algorithms/Q06 - PoliceAndThieves.java
@@ -1,3 +1,57 @@
+✅ Problem: Catch Thieves
+Goal:
+You're given an array of characters of size n. Each character is either:
+
+'P': police
+
+'T': thief
+
+A police can catch one thief if the distance between them is at most k (inclusive).
+Each thief and each police can only be used once.
+
+arr: List of characters, e.g. ['P', 'T', 'T', 'P', 'T']
+n:   Length of arr (int)
+k:   Maximum distance police can reach to catch thief (int)
+class Solution:
+    def catchThieves(self, arr, n, k):
+        i = -1  # pointer to next police
+        j = -1  # pointer to next thief
+        
+        # Initialize i and j
+        for m in range(n):
+            if i == -1 and arr[m] == 'P':
+                i = m
+            if j == -1 and arr[m] == 'T':
+                j = m
+            if i != -1 and j != -1:
+                break
+
+        if i == -1 or j == -1:
+            return 0  # no thief or no police
+
+        caught = 0
+        while i < n and j < n:
+            if abs(i - j) <= k:
+                caught += 1
+                i += 1
+                while i < n and arr[i] != 'P':
+                    i += 1
+                j += 1
+                while j < n and arr[j] != 'T':
+                    j += 1
+            elif i < j:
+                i += 1
+                while i < n and arr[i] != 'P':
+                    i += 1
+            else:
+                j += 1
+                while j < n and arr[j] != 'T':
+                    j += 1
+
+        return caught
+
+
+
 //User function Template for Java
 
 class Solution 
