a21d8872df0a26f18d5af91801f6fe30d11c1057
abressiddique <abressiddique@gmail.com>
2025-07-18 19:31:09 +0530

    Update Q04 - JobSequencingProblem.java

--- Lecture-208 - Greedy Algorithms/Q04 - JobSequencingProblem.java
+++ Lecture-208 - Greedy Algorithms/Q04 - JobSequencingProblem.java
@@ -1,3 +1,47 @@
+class Solution:
+    def JobSequencing(self, id, deadline, profit):
+        # Step 1: Find the maximum deadline
+        n = 0
+        for d in deadline:
+            n = max(n, d)
+
+        # Step 2: Prepare a list to track assigned job slots
+        assignedJobs = [-1] * (n + 1)
+
+        # Step 3: Create an array of indices (0 to len-1)
+        length = len(profit)
+        index = [i for i in range(length)]
+
+        # Step 4: Sort indices by decreasing profit
+        index.sort(key=lambda x: profit[x], reverse=True)
+
+        # Step 5: Try to schedule each job
+        count = 0
+        maxProfit = 0
+        for i in range(length):
+            ind = index[i]
+            d = deadline[ind]
+
+            # Try to find a free slot from deadline d to 1
+            while d > 0 and assignedJobs[d] != -1:
+                d -= 1
+
+            if d == 0:
+                continue
+
+            # Assign job and update counters
+            assignedJobs[d] = id[ind]
+            count += 1
+            maxProfit += profit[ind]
+
+        # Step 6: Return result as a list [number of jobs done, total profit]
+        return [count, maxProfit]
+id = [1, 2, 3, 4]
+deadline = [4, 1, 2, 3]
+profit = [20, 10, 40, 30]
+
+sol = Solution()
+print(sol.JobSequencing(id, deadline, profit))
 
 
 
