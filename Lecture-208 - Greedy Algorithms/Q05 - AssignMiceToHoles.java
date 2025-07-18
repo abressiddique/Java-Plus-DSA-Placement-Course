a591f5d1417c7495d09bdbf25e41eac5100e0497
abressiddique <abressiddique@gmail.com>
2025-07-18 20:20:37 +0530

    Update Q05 - AssignMiceToHoles.java

--- Lecture-208 - Greedy Algorithms/Q05 - AssignMiceToHoles.java
+++ Lecture-208 - Greedy Algorithms/Q05 - AssignMiceToHoles.java
@@ -1,3 +1,21 @@
+
+class Solution:
+    def assignMiceHoles(self, N: int, M: list[int], H: list[int]) -> int:
+        # Step 1: Sort mice and holes
+        M.sort()
+        H.sort()
+        
+        # Step 2: Find max distance for corresponding pairs
+        maxTime = 0
+        for i in range(N):
+            maxTime = max(maxTime, abs(M[i] - H[i]))
+        
+        return maxTime
+sol = Solution()
+print(sol.assignMiceHoles(3, [4, -4, 2], [4, 0, 5]))
+# Output: 4
+
+
 //User function Template for Java
 class Solution {
     static int assignMiceHoles(int N , int[] M , int[] H) {
