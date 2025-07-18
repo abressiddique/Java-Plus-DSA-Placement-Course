1b001e9356eb49dc82c3cf49edc8efa71aee53d9
abressiddique <abressiddique@gmail.com>
2025-07-18 20:43:23 +0530

    Update Q07 - FractionalKnapsack.java

--- Lecture-208 - Greedy Algorithms/Q07 - FractionalKnapsack.java
+++ Lecture-208 - Greedy Algorithms/Q07 - FractionalKnapsack.java
@@ -1,3 +1,40 @@
+class Item:
+    def __init__(self, val, wt):
+        self.val = val
+        self.wt = wt
+        self.ratio = val / wt  # value-to-weight ratio
+
+def fractional_knapsack(values, weights, capacity):
+    n = len(values)
+    
+    # Step 1: Create a list of Items with ratio
+    items = []
+    for i in range(n):
+        items.append(Item(values[i], weights[i]))
+
+    # Step 2: Sort items by ratio in descending order
+    items.sort(key=lambda item: item.ratio, reverse=True)
+
+    # Step 3: Greedily pick items
+    total_value = 0.0
+    for item in items:
+        if capacity == 0:
+            break
+        
+        if item.wt <= capacity:
+            total_value += item.val
+            capacity -= item.wt
+        else:
+            # Take only fraction of the item
+            total_value += item.ratio * capacity
+            capacity = 0
+
+    return total_value
+values = [60, 100, 120]
+weights = [10, 20, 30]
+capacity = 50
+
+print(fractional_knapsack(values, weights, capacity))  # Output: 240.0
 
 
 // User function Template for Java
